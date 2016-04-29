6ff07da4991b935e61ab3693516f0590e6770965
RoCky Wu <rwu@cepave.com>
2016-04-29 18:27:23 +0800

    fixed test spec to `assert.true`

--- test/stop-spec.js
+++ test/stop-spec.js
@@ -1,24 +1,24 @@
 import test from 'ava'
 import stop from '../src'
 
-test('should be run as series', async (assert)=> {
+test('should be run as series', async(assert)=> {
   const start = new Date()
   await stop(2)
   const end = new Date()
 
-  assert.truthy(end - start >= 2000)
+  assert.true(end - start >= 2000)
 })
 
 test('pass 0, calls setImmediate', async(assert)=> {
   const start = new Date()
   await stop(0)
   const end = new Date()
-  assert.truthy(end - start < 17)
+  assert.true(end - start < 17)
 })
 
 test('pass nothing, calls setImmediate', async(assert)=> {
   const start = new Date()
   await stop()
   const end = new Date()
-  assert.truthy(end - start < 17)
+  assert.true(end - start < 17)
 })
