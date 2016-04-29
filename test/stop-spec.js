41aa7c5a1efa8473fec4b9aab0224a3d2aebe413
RoCky Wu <rwu@cepave.com>
2016-04-29 14:23:55 +0800

    implement test spec

--- test/stop-spec.js
+++ test/stop-spec.js
@@ -1,6 +1,24 @@
 import test from 'ava'
-const stop = require('../src')
+import stop from '../src'
 
-test('xxxxxx', (assert)=> {
+test('should be run as series', async (assert)=> {
+  const start = new Date()
+  await stop(2)
+  const end = new Date()
 
+  assert.truthy(end - start >= 2000)
+})
+
+test('pass 0, calls setImmediate', async(assert)=> {
+  const start = new Date()
+  await stop(0)
+  const end = new Date()
+  assert.truthy(end - start < 17)
+})
+
+test('pass nothing, calls setImmediate', async(assert)=> {
+  const start = new Date()
+  await stop()
+  const end = new Date()
+  assert.truthy(end - start < 17)
 })
