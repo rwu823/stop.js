e54e781c6192cf7e04dcaf8f90a8f2bdc82ef465
Federico Brigante <bfred-it@users.noreply.github.com>
2016-05-16 23:45:50 -0700

    Remove manual type conversion
    isNaN and setTimeout automatically do that

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ require('setimmediate')
 module.exports = (ms = 0)=> {
   let _ms = ms
 
-  if ('string' === typeof _ms) _ms = +ms
-
   if (isNaN(_ms)) throw new Error(`${ms} is not a valid number`)
   
   return new Promise((resolve, reject) => {
